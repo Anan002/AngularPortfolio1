5acd6ac608f81c86bf173b5939d25a8efd8bcdfe
jsm33t <jskainthofficial@gmail.com>
2024-08-25 21:34:56 +0530

    :art: add ham menu button

---
+++ src/app/shared/icons/ham-menu-icon/ham-menu-icon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ham-menu-icon',
+  templateUrl: './ham-menu-icon.component.html'
+})
+export class HamMenuIconComponent {
+
+}
