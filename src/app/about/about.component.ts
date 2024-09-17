c955f1fa77ab7b9a3b52e4a3c027c0c2027f5f8a
Anan002 <anandanhammed002@gmail.com>
2024-09-17 19:55:59 +0530

    created the about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  name = 'Anandan';
+  professionalDescription = 'Experienced dancer with a passion for creating scalable and efficient applications.';
+  personalDescription = 'When I’m not coding, I’m hitting the gym. Fitness is a huge part of my life, and I love pushing my limits.';
+
+  viewMore() {
+    
+  }
+
+  seeMore() {
+    
+  }
 }
