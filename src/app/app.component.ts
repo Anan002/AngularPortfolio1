356c1d3d49608eba60fba6838dab32e72a12d9be
Anan002 <anandanhammed002@gmail.com>
2024-09-19 22:56:38 +0530

    implemented loader animation and cart animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-// src/app/app.component.ts
 import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Anandandevprotfolio';
+  isLoading = false;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationStart || event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError))
+      .subscribe(event => {
+        if (event instanceof NavigationStart) {
+          this.isLoading = true; // Start loader
+          console.log('Navigation started');
+        } else {
+          this.isLoading = false; // Stop loader
+          console.log('Navigation ended or canceled');
+        }
+      });
+  }
+  ngOnInit() {
+    // Show loader for 2 seconds on initial load
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false; // Hide loader after 2 seconds
+    }, 100000);
+  }
 }
