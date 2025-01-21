0be0e97218d3beef59b5e8441213842008466812
Anan002 <anandanhammed002@gmail.com>
2025-01-21 21:42:33 +0530

    added the deatils of other projects

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -16,37 +16,37 @@ interface Project {
 export class PortfolioComponent implements AfterViewInit {
   projects: Project[] = [
     {
-      title: 'NRL MOC Process',
+      title: 'NRL MOC Process - Client',
       description:
         'Numaligarh Refinery Limited (NRL) is an oil company with various departments. The Management of Change (MOC) process involves transferring a set of documents from one department to another, with approval confirmation and the completion of forms based on the role.',
       image: 'assets/Project Images/nrl.png',
     },
     {
-      title: 'DB corp Sales App',
+      title: 'DB corp Sales App - Client',
       description:
         'Dainik Bhaskar is one of the largest newspaper companies in India. For effective inside sales management, the company uses a sales app to maintain client details, track call status, and ensure smooth communication. The app includes various features like lead management, reporting, and performance tracking to enhance sales efficiency.',
       image: 'assets/Project Images/danik bhaskar.jpg',
     },
     {
-      title: 'Reval ERP',
+      title: 'Reval ERP - Product',
       description:
         'Revalsys is an innovative software development and support company. Their flagship product, Reval ERP (Enterprise Resource Planning), includes modules for HR, sales, supply chain, finance, accounting, and more. The ERP system integrates all the essential tools and processes required to efficiently manage and run a successful company.',
       image: 'assets/Project Images/Revalsys.jpeg',
     },
     {
-      title: 'IIB India',
+      title: 'IIB India - Client',
       description:
         'The Insurance Information Bureau (IIB) of India is an organization that holds detailed information about insurance policies across various sectors. It maintains a comprehensive and secure database of insurance data, updated annually. The IIB plays a crucial role in managing the flow of insurance data to support regulatory functions.',
       image: 'assets/Project Images/IIB.jpeg',
     },
     {
-      title: 'Project Five',
-      description: 'This is a brief description of Project Five.',
-      image: 'assets/Project Images/Watch station.png',
+      title: 'Reval Remittance - Product',
+      description: 'Revalsys is an innovative software company with its key product, Reval Remittance. It automates retail amount verification for small stores, comparing sales with e-commerce data. Users simply provide credentials, and the system processes reports and sends results via email.',
+      image: 'assets/Project Images/Revalsys.jpeg',
     },
     {
-      title: 'Project Five',
-      description: 'This is a brief description of Project Five.',
+      title: 'Hopp Valet - Client',
+      description: 'Hopp is a driver app with a Valet Request feature. This functionality enables users to assign personnel for car parking efficiently. It simplifies the car parking process through easy request handling.The app ensures convenience and smooth parking management.',
       image: 'assets/Project Images/hopp.jpeg',
     },
   ];
